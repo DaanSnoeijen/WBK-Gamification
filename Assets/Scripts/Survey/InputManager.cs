67424f6d20267258edbd60f4c27f7bdd83bcf00a
DaanSnoeijen <438919@student.fontys.nl>
2023-10-05 15:05:04 +0200

    Toggle user input field

--- Assets/Scripts/Survey/InputManager.cs
+++ Assets/Scripts/Survey/InputManager.cs
@@ -2,12 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InputManager : MonoBehaviour
 {
     [Header("Able to send messages")]
     [SerializeField] MessageCreator MessageCreator;
     [SerializeField] TMP_InputField InputField;
+
+    [Header("Input field toggling")]
+    [SerializeField] Image SendButtonImage;
+    [SerializeField] Image SendButtonBackImage;
+    [SerializeField] Button SendButton;
+
+    Color ButtonOnColor = new Color(0f, 0.635f, 0.909f);
+    Color ButtonOffColor = new Color(0.29f, 0.29f, 0.29f);
+
+    Color ButtonBackOnColor = new Color(0.247f, 0.282f, 0.8f);
+    Color ButtonBackOffColor = new Color(0.211f, 0.211f, 0.211f);
 
     public void SendUserMessage()
     {
@@ -18,8 +30,20 @@ public class InputManager : MonoBehaviour
         }
     }
 
-    public void AbleToSend(bool state)
+    public void ToggleSending(bool state)
     {
-        //Set if user is able to send a message or continue through the send button
+        InputField.enabled = state;
+        SendButton.enabled = state;
+
+        if (state)
+        {
+            SendButtonImage.color = ButtonOnColor;
+            SendButtonBackImage.color = ButtonBackOnColor;
+        }
+        else
+        {
+            SendButtonImage.color = ButtonOffColor;
+            SendButtonBackImage.color = ButtonBackOffColor;
+        }
     }
 }
