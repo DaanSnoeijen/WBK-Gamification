c75bbfee7c693d4274d2caac28cd8e192c448e31
DaanSnoeijen <438919@student.fontys.nl>
2023-10-06 10:23:37 +0200

    Correct UI and message scaling

---
+++ Assets/Scripts/QualitySettings.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QualitySettings : MonoBehaviour
+{
+    void Start()
+    {
+        Application.targetFrameRate = 60; 
+    }  
+}
