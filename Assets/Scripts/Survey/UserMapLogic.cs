094494bb112c5ac070bc8780540b43e8c37ead2e
DaanSnoeijen <438919@student.fontys.nl>
2023-10-25 10:27:21 +0200

    Added map instructions

--- Assets/Scripts/Survey/UserMapLogic.cs
+++ Assets/Scripts/Survey/UserMapLogic.cs
@@ -1,12 +1,19 @@
+using Mono.Cecil.Cil;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.UIElements;
 
 public class UserMapLogic : MonoBehaviour
 {
     [Header("Object with point image on map")]
     [SerializeField] RectTransform Pointer;
+
+    [Header("Move instructions for map")]
+    [SerializeField] GameObject Instructions;
+
+    public void SetInstructions(bool show) { Instructions.SetActive(show); }
 
     public void SetPointer()
     {
