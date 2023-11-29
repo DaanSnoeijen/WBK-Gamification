abd567d87402c90f64e21f278134a482b449f618
DaanSnoeijen <438919@student.fontys.nl>
2023-11-29 14:42:22 +0100

    Added announcements

---
+++ Assets/Scripts/Main/LoadSurvey.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadSurvey : MonoBehaviour
+{
+    public void EnterSurvey() { SceneManager.LoadScene(0); }
+}
