2c6bf6581ff25d4dc5e789c25a79460092b31c62
DaanSnoeijen <438919@student.fontys.nl>
2024-01-26 10:00:53 +0100

    Onboarding finished

--- Assets/Scripts/Main/OnboardingManager.cs
+++ Assets/Scripts/Main/OnboardingManager.cs
@@ -3,15 +3,21 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class OnboardingManager : MonoBehaviour
 {
     [Header("Onboarding screens")]
     [SerializeField] List<string> DescriptionItems;
 
-    [Header("Progressbar settings")]
+    [Header("Onboarding items")]
     [SerializeField] GameObject MainDot;
     [SerializeField] TextMeshProUGUI Description;
+    [SerializeField] TextMeshProUGUI ButtonText;
+    [SerializeField] Button SwitchRButton;
+    [SerializeField] Button SwitchLButton;
+
+    [Header("Animators")]
     [SerializeField] Animator FocusAnimator;
     [SerializeField] Animator ArrowAnimator;
     [SerializeField] Animator DotAnimator;
@@ -42,8 +48,15 @@ public class OnboardingManager : MonoBehaviour
 
         if (dotStep == 0) ButtonLAnimator.SetTrigger("Hide");
         else if (right && dotStep == 1) ButtonLAnimator.SetTrigger("Show");
-        if (dotStep == DescriptionItems.Count - 1) ButtonRAnimator.SetTrigger("Hide");
+        if (dotStep == DescriptionItems.Count - 1)
+        {
+            ButtonRAnimator.SetTrigger("Hide");
+            ButtonText.text = "Begrepen";
+        }
         else if (!right && dotStep == DescriptionItems.Count - 2) ButtonRAnimator.SetTrigger("Show");
+
+        if (dotStep == 5 && right) SwitchRButton.onClick.Invoke();
+        else if (dotStep == 4 && !right) SwitchLButton.onClick.Invoke();
 
         StartCoroutine(IMoveMainDot(right));
         StartCoroutine(IChangeDescription());
@@ -53,6 +66,7 @@ public class OnboardingManager : MonoBehaviour
     {
         PanelAnimator.SetTrigger("Hide");
         Screens.SetActive(false);
+        SwitchLButton.onClick.Invoke();
     }
     
     IEnumerator IMoveMainDot(bool right)
