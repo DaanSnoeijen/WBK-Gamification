b5b30c7844ea2a41b334cb7e896b1a7dbbf1b713
DaanSnoeijen <438919@student.fontys.nl>
2024-01-31 09:51:45 +0100

    Own marker menu and remove

---
+++ Assets/Scripts/Main/DeleteObject.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeleteObject : MonoBehaviour
+{
+    public void DestroyObject(GameObject objectToDestroy) { Destroy(objectToDestroy); }
+}
